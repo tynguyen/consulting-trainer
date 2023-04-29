b00b59c69e7446506031f93767257fb57d186cca
Néstor Nicolás Campos Rojas <nescampos_18@hotmail.com>
2023-04-29 18:11:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,40 @@ from langchain.schema import (
     BaseMessage,
 )
 import os
-import CAMELAgent
+
+class CAMELAgent:
+    def __init__(
+        self,
+        system_message: SystemMessage,
+        model: ChatOpenAI,
+    ) -> None:
+        self.system_message = system_message
+        self.model = model
+        self.init_messages()
+
+    def reset(self) -> None:
+        self.init_messages()
+        return self.stored_messages
+
+    def init_messages(self) -> None:
+        self.stored_messages = [self.system_message]
+
+    def update_messages(self, message: BaseMessage) -> List[BaseMessage]:
+        self.stored_messages.append(message)
+        return self.stored_messages
+
+    def step(
+        self,
+        input_message: HumanMessage,
+    ) -> AIMessage:
+        messages = self.update_messages(input_message)
+
+        output_message = self.model(messages)
+        self.update_messages(output_message)
+
+        return output_message
+
+
 
 consultant_inception_prompt = (
 """Never forget you are a {you_role_name} and I am a {customer_role_name}. Never flip roles! Never instruct me!
